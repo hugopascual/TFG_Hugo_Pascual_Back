79a19c9f5a9aad25c01579a24e2ec91d75b1a686
Hugo Pascual <hugo.pascual.adan@gmail.com>
2020-12-28 12:20:24 +0100

    first try with JSONs and send them

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
 
 var app = express();
 
@@ -15,12 +15,12 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/home.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home */
+router.get('/', function(req, res, next) {
+    status = HTTP_OK;
+    fileName = "./resources/home.json";
+
+    setTimeout(function () {
+        sendResponse(fileName, req, res, status);
+    }, timeWaiting);
+});
+
+//constants
+//constants and variables definition
+var showRequest = false;
+var showResponse = true;
+
+var timeWaiting = 1000;
+
+const HTTP_OK = 200;
+/*
+const HTTP_NO_CONTENT = 204;
+const HTTP_BAD_REQUEST = 400;
+const HTTP_UNAUTHORIZED = 401;
+const HTTP_FORBIDDEN = 403;
+const HTTP_NOT_FOUND = 404;
+const HTTP_CONFLICT = 409;
+const HTTP_PRECONDITION_FAILED = 412;
+const TOKEN = "TokenGeneratedByHeroku";
+ */
+
+//useful functions
+function sendResponse(fileName, req, res, status) {
+    console.log("***JSON: " + fileName);
+    console.log("***PARAMS: " + req.params);
+
+    if (showRequest) {
+        console.log("***Request.body:");
+        console.log(req.body);
+    }
+
+    if (fs.existsSync(fileName)) {
+        // Do something
+        fs.readFile(fileName, 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                var obj = JSON.parse(data);
+                res.header('Content-Type', 'application/json');
+
+                if (showResponse) {
+                    console.log("***Response: ");
+                    console.log(obj);
+                    console.log("***Status response:");
+                    console.log(status);
+                }
+                res.status(status).json(obj);
+            }
+        });
+    } else {
+        console.log("not found");
+        fs.readFile("error.json", 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                var obj = JSON.parse(data);
+                res.header('Content-Type', 'application/json');
+                res.status(status).json(obj);
+            }
+        });
+    }
+}
+
+module.exports = router;
