5bc0f180e53fd45842c128d575ee5265d682bf5c
Hugo Pascual <hugo.pascual.adan@gmail.com>
2021-02-09 20:46:34 +0100

    getUsers y createUser

--- api/user.js
+++ api/user.js
@@ -9,7 +9,7 @@ exports.index = async (req, res, next) => {
 
     try {
         const users = await models.User.findAll({
-            attributes: ['id', 'email', 'username', 'password', 'token', 'createdAt', 'updatedAt']
+            attributes: ['id', 'email', 'username']
         });
         res.json(users);
     } catch (error) {
